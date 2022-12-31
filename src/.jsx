46182fd2743815a8a6c0693529d9473a99176a71
Mathis <mathis2007voisin@gmail.com>
2022-12-31 16:10:19 +0100

    File of Forum

---
+++ src/.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+var header = (
+
+    <h1>
+    RegLang Forum 
+    </h1>
+
+
+);
+
+ReactDOM.render(
+    header,
+    document.getElementById
+)
